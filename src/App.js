6af3ccf21be8bdaddaf5495aa5eb568a7641f3e8
Raul Perez <zerepangel@gmail.com>
2016-11-14 20:44:03 +0100

    Hello, world title

--- src/App.js
+++ src/App.js
@@ -5,45 +5,24 @@ import Title from './Title';
 
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      text: "text"
+      message: "Hello, World!"
     };
-
-    this.handler = this.handler.bind(this);
-  }
-
-  handler() {
-    this.setState({text: "clikeeeeee"});
-  }
+  } 
 
   render() {
-    const list = [1,2,3,4,5,6,7];
-    const yo = list.map(function(number) {
-      return <Title headline={number}/>;
-    });
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2 onClick={() => this.setState({message: this.state.message + '!'})}>{this.state.message}</h2>
         </div>
-
-        {yo}
-
-        <Title headline="top title"/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div>
-          <div>{this.state.text}</div>
-          <input/>
-          <button onClick={this.handler}>click me</button>
-          <div>{this.state.text}</div>
-
-        </div>
-        <Title headline="bottom title"/>
 
       </div>
     );
